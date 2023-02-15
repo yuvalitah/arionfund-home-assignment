15848616082fadb91d45b487afd10773b55a52fe
Yuval Itah <yuval18998@gmail.com>
2023-02-15 17:32:07 +0200

    Change DatePicker component styling

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { TextField, Box } from "@mui/material";
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { TextField, Box, useMediaQuery, useTheme } from "@mui/material";
+import { DesktopDatePicker, MobileDatePicker } from "@mui/x-date-pickers";
 import moment from "moment";
 import { APP_BAR_HEIGHT } from "../../theme";
 
@@ -10,6 +10,7 @@ interface IDatePickerProps {
 
 export const DatePicker = ({ resetPictures }: IDatePickerProps) => {
   const [time, setTime] = useState(moment().format());
+  const theme = useTheme();
 
   const handleTimeChange = (value: string | null) => {
     if (value) {
@@ -19,12 +20,24 @@ export const DatePicker = ({ resetPictures }: IDatePickerProps) => {
   };
 
   return (
-    <Box position="fixed" top={APP_BAR_HEIGHT}>
-      <DesktopDatePicker
-        onChange={handleTimeChange}
-        value={time}
-        renderInput={(params) => <TextField {...params} />}
-      />
+    <Box position="fixed" top={APP_BAR_HEIGHT + 20}>
+      {useMediaQuery(theme.breakpoints.up("sm")) ? (
+        <DesktopDatePicker
+          label="Date"
+          onChange={handleTimeChange}
+          value={time}
+          renderInput={(params) => <TextField variant="filled" {...params} />}
+          inputFormat="DD/MM/YYYY"
+        />
+      ) : (
+        <MobileDatePicker
+          label="Date"
+          onChange={handleTimeChange}
+          value={time}
+          renderInput={(params) => <TextField variant="filled" {...params} />}
+          inputFormat="DD/MM/YYYY"
+        />
+      )}
     </Box>
   );
 };
--- src/theme/Theme.ts
+++ src/theme/Theme.ts
@@ -1,4 +1,5 @@
 import { createTheme as createMuiTheme, PaletteMode } from "@mui/material";
+import type {} from "@mui/x-date-pickers/themeAugmentation";
 
 export const APP_BAR_HEIGHT = 64;
 
@@ -12,6 +13,19 @@ export const createTheme = (mode: PaletteMode) =>
         styleOverrides: {
           positionStatic: {
             height: APP_BAR_HEIGHT,
+          },
+        },
+      },
+      MuiFilledInput: {
+        styleOverrides: {
+          root: {
+            backgroundColor: mode === "light" ? "#dfedff" : "#1A2027",
+            ":hover": {
+              backgroundColor: mode === "light" ? "#dfedff" : "#1A2027",
+            },
+            "&.Mui-focused": {
+              backgroundColor: mode === "light" ? "#dfedff" : "#1A2027",
+            },
           },
         },
       },
